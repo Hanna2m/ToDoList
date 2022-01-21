447f5d1298bcadff76cce77dca44290a6a2bec53
Rachael Kunz <rachaelmkunz@gmail.com>
2022-01-21 12:09:24 +0100

    add edit button functionality

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-
 function add() {
+  // add button text change back to add
+  document.getElementById("addBtn").innerHTML = "add";
 
   let li = document.createElement("LI");
   let input_value = document.form_main.task.value;
   let input_text = document.createTextNode(input_value);
-  input_value.id = "input"
- 
+  input_value.id = "input";
+
   //checkbox
- 
+
   let checkBox = document.createElement("span");
   checkBox.id = "check-icon";
   checkBox.className = "material-icons";
@@ -16,22 +17,20 @@ function add() {
   checkBox.style.color = "#DDDDDD";
   checkBox.appendChild(checkTxt);
   li.appendChild(checkBox);
-  
-  
+
   li.appendChild(input_text);
   document.querySelector("ul").appendChild(li);
   document.form_main.task.value = "";
 
   //delete Btn
   let deleteButton = document.createElement("SPAN");
-  deleteButton.className = "material-icons"
+  deleteButton.className = "material-icons";
   let txt = document.createTextNode("delete");
   deleteButton.id = "delete-btn";
   deleteButton.style.height = "24px";
   deleteButton.style.color = "red";
   deleteButton.appendChild(txt);
   li.appendChild(deleteButton);
-
 
   // edit button
 
@@ -43,68 +42,48 @@ function add() {
   editTaskButton.style.color = "#DDDDDD";
   editTaskButton.appendChild(editTxt);
   li.appendChild(editTaskButton);
-  
-  
-  //delete task
-  deleteButton.addEventListener("click", (e) => 
-  (e.target.parentElement.style.display = "none"));
 
+  //delete task
+  deleteButton.addEventListener(
+    "click",
+    (e) => (e.target.parentElement.style.display = "none")
+  );
 
   // edit task
   editTaskButton.onclick = (e) => {
-    console.log("edit button clicked");
+    let editText = document.getElementsByName("task")[0];
+    console.log(input_value);
+    editText.placeholder = input_value;
+    e.target.parentElement.classList.add("hidden");
+    document.getElementById("addBtn").innerHTML = "edit";
+  };
 
-  e.target.parentElement.classList.add("label");
-    // when user clicks edit:
-    // assign current value of the list item to a variable
-  console.log(e.target.parentElement);
-
-    let listItems = document.querySelectorAll(".label");
-    console.log(e.target.parentElement.innerText);
-    console.log(listItems);
-    listItems.forEach((element) => {
-      console.log(element);
-      if (element === e.target) {
-        console.log("targeted");
-      }
-    });
-
-    // show input box and allow edit
-    // edit button changes to save button
-    // when user clicks save button;
-    // updated item appears in list
-    // save button changes to edit button
-  // };
-  
-  return li;
-}
-
-//adding press enter functionality
-document.getElementById('task').addEventListener('keypress', function (e) {
-  if (e.keyCode === 13 || e.which === 13) {
+  //adding press enter functionality
+  document.getElementById("task").addEventListener("keypress", function (e) {
+    if (e.keyCode === 13 || e.which === 13) {
       e.preventDefault();
       add();
       return false;
-  }
-});
-//end press enter functionality
+    }
+  });
+  //end press enter functionality
 
-//completed task
+  //completed task
 
+  let taskCompleted = document.getElementById("completed-tasks");
+  taskCompleted.hidden = true;
+  let completedTasksHolder = document.getElementById("completed-tasks");
 
-let taskCompleted = document.getElementById("completed-tasks");
-taskCompleted.hidden = true;
-let completedTasksHolder = document.getElementById("completed-tasks");
+  document.querySelector("ul").addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
+      let item = e.target;
+      taskCompleted.hidden = false;
+      document.getElementById("delete-btn").style.display = "none";
+      document.getElementById("edit-btn").style.display = "none";
+      document.getElementById("check-icon").style.color = "green";
+      taskCompleted.appendChild(item);
+    }
+  });
 
-document.querySelector("ul").addEventListener("click", (e) => {
-  if (e.target.tagName === "LI") {
-    let item = e.target
-    taskCompleted.hidden = false;
-    document.getElementById("delete-btn").style.display = "none";
-    document.getElementById("edit-btn").style.display = "none";
-    document.getElementById("check-icon").style.color = "green"
-    taskCompleted.appendChild(item)
-    
-  
- }
-});}
+  return li;
+}
